75f3a65510551c0e63a594d84a25f8080472796d
zhangm19910220@gmail.com <zhangm19910220@gmail.com>
2019-07-09 17:52:52 +0800

    Add new dependencies, remove some unuseless code and add font-family

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # admin-skeleton
 
-## Project setup
+## 1. Project setup
 ```
 npm install
 ```
@@ -27,3 +27,40 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 2. 功能简介及规划
+
+### **基础功能**
+- 工程基础布局
+- Vue-router 路由集成
+- Vue-store 全局状态管理
+- Axios 集成
+- iconfont 第三方图标库集成
+- i18n 多语言维护
+- 前端数据字典翻译
+- 前端资源权限控制
+- 前端样式主题维护
+```
+前端字体选型
+https://segmentfault.com/a/1190000006110417
+```
+### **组件集成**
+- Element 组件库集成
+- Element Table 及 Form 组件二次封装
+### **第三方封装组件**
+- 富文本编辑器
+- Markdown 编辑器
+- Codemirror 编辑器
+- others
+### **Mock.JS 请求拦截**
+- 以 mock.js 为模拟数据，提供前端请求数据
+```
+http://mockjs.com/
+```
+### **其它**
+- 前端代码编写规范
+- Git 团队协作规范
+- 前端工程配套文档维护
+- 前端兼容性
+- 服务器自动部署脚本
+- Vscode 插件选择及配置
--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="less">
+<style lang="less" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  // https://segmentfault.com/a/1190000006110417
+  font-family: "Helvetica Neue", Helvetica, Arial, "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
