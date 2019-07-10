f523dbbbea7949399b74256d5835af47f9c2767f
zhangm19910220@gmail.com <zhangm19910220@gmail.com>
2019-07-10 09:50:46 +0800

    Change ElementUI border-radius base to 2px

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view />
+</template>
+<script>
+export default {
+  name: 'Home',
+  created () {
+    console.log('Home Created')
+  },
+  mounted () {
+    console.log('Home Mounted')
+  }
+}
+</script>
